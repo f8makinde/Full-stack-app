9948db18cebd8ebcf87fdea1a3974d107ad1b164
Faith Makinde <faithmakinde0@gmail.com>
2024-01-01 13:14:33 +0100

    navbar completed

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from "./about.module.css"
+const AboutPage = () => {
+  return (
+    <div className={styles.imgContainer}>
+      <Image src="/about.png" alt="about" fill/>
+    </div>
+  )
+}
+
+export default AboutPage
